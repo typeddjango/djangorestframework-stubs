a01ad868226796a4fecbdbf005ab28c819e0d61e
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2018-11-13 19:05:04 +0300

    add stubs for some fields

--- rest_framework-stubs/fields.pyi
+++ rest_framework-stubs/fields.pyi
@@ -1,4 +1,5 @@
-from typing import Any, Union, Callable, Mapping, Dict
+import datetime
+from typing import Any, Union, Callable, Mapping, Dict, NoReturn, Optional, List
 
 
 class Field(object):
@@ -9,13 +10,15 @@ class Field(object):
                  default: Any = ...,
                  initial: Any = ...,
                  source: Union[Callable, str] = ...,
-                 help_text: bool = ...,
+                 help_text: str = ...,
                  allow_null: bool = ...,
                  **kwargs): ...
 
     def run_validation(self, data: Mapping[str, Any] = ...) -> Dict[str, Any]: ...
 
     def __get__(self, instance, owner) -> Any: ...
+
+    def fail(self, key: str, **kwargs) -> NoReturn: ...
 
 
 class CharField(Field):
@@ -26,7 +29,7 @@ class CharField(Field):
                  default: Any = ...,
                  initial: Any = ...,
                  source: Union[Callable, str] = ...,
-                 help_text: bool = ...,
+                 help_text: str = ...,
                  allow_null: bool = ...,
                  allow_blank: bool = ...,
                  trim_whitespace: bool = ...,
@@ -35,3 +38,33 @@ class CharField(Field):
                  **kwargs): ...
 
     def __get__(self, instance, owner) -> str: ...
+
+class RegexField(Field):
+    def __get__(self, instance, owner) -> str: ...
+
+class EmailField(Field):
+    def __get__(self, instance, owner) -> str: ...
+
+class IntegerField(Field):
+    def __get__(self, instance, owner) -> int: ...
+
+class BooleanField(Field):
+    def __get__(self, instance, owner) -> bool: ...
+
+class NullBooleanField(Field):
+    def __get__(self, instance, owner) -> Optional[bool]: ...
+
+class ListField(Field):
+    def __get__(self, instance, owner) -> List[Any]: ...
+
+class DictField(Field):
+    def __get__(self, instance, owner) -> Dict[str, Any]: ...
+
+class ChoiceField(Field):
+    def __get__(self, instance, owner) -> Any: ...
+
+class JSONField(Field):
+    def __get__(self, instance, owner) -> Any: ...
+
+class DateTimeField(Field):
+    def __get__(self, instance, owner) -> datetime.datetime: ...
--- rest_framework-stubs/serializers.pyi
+++ rest_framework-stubs/serializers.pyi
@@ -1,5 +1,15 @@
-from rest_framework.fields import (CharField as CharField,
-                                   Field as Field)
+from rest_framework.fields import (Field as Field,
+                                   CharField as CharField,
+                                   RegexField as RegexField,
+                                   EmailField as EmailField,
+                                   IntegerField as IntegerField,
+                                   BooleanField as BooleanField,
+                                   NullBooleanField as NullBooleanField,
+                                   ListField as ListField,
+                                   DictField as DictField,
+                                   ChoiceField as ChoiceField,
+                                   JSONField as JSONField,
+                                   DateTimeField as DateTimeField)
 
 
 class Serializer(Field):
