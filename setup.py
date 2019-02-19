a66adb0e7c7ecdd69f4bfbd2e8831e7ad229ceb9
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-02-19 03:01:21 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,51 @@
 import os
+import sys
 from distutils.core import setup
 
+from setuptools import find_packages
+
+
+def find_stub_files(name):
+    result = []
+    for root, dirs, files in os.walk(name):
+        for file in files:
+            if file.endswith('.pyi'):
+                if os.path.sep in root:
+                    sub_root = root.split(os.path.sep, 1)[-1]
+                    file = os.path.join(sub_root, file)
+                result.append(file)
+    return result
+
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+dependencies = [
+    'djangorestframework>=3.9.0'
+    'mypy>=0.670',
+    'typing-extensions'
+]
+if sys.version_info[:2] < (3, 7):
+    # dataclasses port for 3.6
+    dependencies += ['dataclasses']
 
 setup(
     name="djangorestframework-stubs",
-    url="https://github.com/mkurnikov/djangorestframework-stubs.git",
+    version="0.1.0",
+    description='PEP-484 stubs for django-rest-framework',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://github.com/mkurnikov/djangorestframework-stubs",
     author="Maksim Kurnikov",
     author_email="maxim.kurnikov@gmail.com",
-    version="0.1.0",
-    license='BSD',
-    install_requires='djangorestframework>=3.9.0',
-    packages=['mypy_rest_framework_plugin']
+    license='MIT',
+    install_requires=dependencies,
+    packages=['djangorestframework-stubs', *find_packages()],
+    package_data={'djangorestframework-stubs': find_stub_files('djangorestframework-stubs')},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
 )
