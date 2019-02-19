9215c1a35e3b04c1d371c28b0de905f5cbadbf2f
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-02-19 03:26:34 +0300

    add generics, mixins

--- rest_framework-stubs/status.pyi
+++ rest_framework-stubs/status.pyi
@@ -1,11 +1,3 @@
-"""
-Descriptive HTTP status codes, for code readability.
-
-See RFC 2616 - https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-And RFC 6585 - https://tools.ietf.org/html/rfc6585
-And RFC 4918 - https://tools.ietf.org/html/rfc4918
-"""
-
 def is_informational(code: int) -> bool: ...
 def is_success(code: int) -> bool: ...
 def is_redirect(code: int) -> bool: ...
