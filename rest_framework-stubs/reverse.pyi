c4b4bda32141cdb7843c35ae0e79c8e8f19f96de
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-02-27 20:11:00 +0300

    add apps, decorators, reverse

---
+++ rest_framework-stubs/reverse.pyi
@@ -0,0 +1,23 @@
+from typing import Any, Callable, Mapping, Optional, Sequence
+
+from django.http import HttpRequest
+
+def preserve_builtin_query_params(url: str, request: Optional[HttpRequest] = ...) -> str: ...
+def reverse(
+    viewname: str,
+    args: Optional[Sequence[Any]] = ...,
+    kwargs: Optional[Mapping[str, Any]] = ...,
+    request: Optional[HttpRequest] = ...,
+    format: Optional[str] = ...,
+    **extra: Any
+) -> str: ...
+def _reverse(
+    viewname: str,
+    args: Optional[Sequence[Any]] = ...,
+    kwargs: Optional[Mapping[str, Any]] = ...,
+    request: Optional[HttpRequest] = ...,
+    format: Optional[str] = ...,
+    **extra: Any
+) -> str: ...
+
+reverse_lazy: Callable[[...], str] = ...
