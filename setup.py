50f4c635a21ec9ceb61514a9297117d120312372
sobolevn <mail@sobolevn.me>
2020-07-03 15:13:58 +0300

    Adds setup.py to black

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def find_stub_files(name):
     result = []
     for root, dirs, files in os.walk(name):
         for file in files:
-            if file.endswith('.pyi'):
+            if file.endswith(".pyi"):
                 if os.path.sep in root:
                     sub_root = root.split(os.path.sep, 1)[-1]
                     file = os.path.join(sub_root, file)
@@ -16,32 +16,28 @@ def find_stub_files(name):
     return result
 
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     readme = f.read()
 
-dependencies = [
-    'mypy>=0.750,<0.780',
-    'django-stubs>=1.3.0',
-    'typing-extensions'
-]
+dependencies = ["mypy>=0.750,<0.780", "django-stubs>=1.3.0", "typing-extensions"]
 
 setup(
     name="djangorestframework-stubs",
     version="1.2.0",
-    description='PEP-484 stubs for django-rest-framework',
+    description="PEP-484 stubs for django-rest-framework",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/typeddjango/djangorestframework-stubs",
     author="Maksim Kurnikov",
     author_email="maxim.kurnikov@gmail.com",
-    license='MIT',
+    license="MIT",
     install_requires=dependencies,
-    packages=['rest_framework-stubs', *find_packages(exclude=['scripts'])],
-    package_data={'rest_framework-stubs': find_stub_files('rest_framework-stubs')},
+    packages=["rest_framework-stubs", *find_packages(exclude=["scripts"])],
+    package_data={"rest_framework-stubs": find_stub_files("rest_framework-stubs")},
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-    ]
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
