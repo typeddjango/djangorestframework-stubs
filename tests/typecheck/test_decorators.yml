typeddjango/djangorestframework-stubs

commit cf9d492ec01f99e9a3fa89558aa1fbf1a749bc22

Sync, update and debug against DRF (#94)
* resolved flake8 issues

* Revert "resolved flake8 issues"

This reverts commit 1a3692c8ac8260a4a51208a04cb223ad882a2558.

* addressed review comments

* resolved flake8 issues

* Added missing stubs and data

* fixed tests

* applied black and isort

* converted Literal import from typing to typing_extensions

* updated compat

* second pass

* Update relations.pyi

* Update utils.pyi

* Update utils.pyi

* Update serializers.pyi

* refined typings

* fixed schema typings

* fix wrong typing of request

* added functional testing script

* updated configs

* functioning version

* updated implementation

* fixed issues following drf test

* updated serializer typing to use generics

* fixed relations and permissions

* fixed parsers

* fixed pagination

* fixed serializers

* update fields

* updated fields and added protocol to serializer

* fixed field typing to use generic

* updated relations

* finished serializers

* finished fields

* fixed decorators

* fixed authtoken

* fixed generic relations

* fixed mypy detected bugs

* finished schema

* finished fixing all drf related errors

* cleanup

* fix mypy.ini

* fixed tests

* updated travis settings

* updated readme

* temporarily disabled new travis config

* fixed python 3.6 comp. issues

* re-enabled full travis config

* address has_object_permission review comment

* fixed version comp issue

* address permission review comment

* applied black and isort

* update typecheck script to use distutils

* isorted

* cleanup

* Update settings.pyi

* addressed review comments

* Update decorators.pyi

rest_framework-stubs/decorators.pyi
@@ -1,29 +1,13 @@
-from typing import Any, Callable, Optional, Sequence, Type, TypeVar, Union
+from typing import Any, Callable, List, Mapping, Optional, Sequence, Type, Union
 
-from django.http.response import HttpResponseBase
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.parsers import BaseParser
 from rest_framework.permissions import BasePermission
 from rest_framework.renderers import BaseRenderer
 from rest_framework.schemas.inspectors import ViewInspector
 from rest_framework.throttling import BaseThrottle
-
-_VIEW = TypeVar("_VIEW", bound=Callable[..., HttpResponseBase])
-
-def api_view(http_method_names: Optional[Sequence[str]] = ...) -> Callable[[_VIEW], _VIEW]: ...
-def renderer_classes(renderer_classes: Sequence[Type[BaseRenderer]]) -> Callable[[_VIEW], _VIEW]: ...
-def parser_classes(parser_classes: Sequence[Type[BaseParser]]) -> Callable[[_VIEW], _VIEW]: ...
-def authentication_classes(authentication_classes: Sequence[Type[BaseAuthentication]]) -> Callable[[_VIEW], _VIEW]: ...
-def throttle_classes(throttle_classes: Sequence[Type[BaseThrottle]]) -> Callable[[_VIEW], _VIEW]: ...
-def permission_classes(permission_classes: Sequence[Type[BasePermission]]) -> Callable[[_VIEW], _VIEW]: ...
-def schema(view_inspector: Optional[Type[ViewInspector]]) -> Callable[[_VIEW], _VIEW]: ...
-def action(
-    methods: Optional[Sequence[str]] = ...,
-    detail: Optional[bool] = ...,
-    url_path: Optional[str] = ...,
-    url_name: Optional[str] = ...,
-    **kwargs: Any
-) -> Callable[[_VIEW], _VIEW]: ...
+from rest_framework.views import APIView, AsView  # noqa: F401
+from typing_extensions import Literal
 
 class MethodMapper(dict):
     def __init__(self, action: Callable, methods: Sequence[str]) -> None: ...
@@ -37,5 +21,67 @@ class MethodMapper(dict):
     def options(self, func: Callable) -> Callable: ...
     def trace(self, func: Callable) -> Callable: ...
 
-def detail_route(methods: Optional[Sequence[str]] = ..., **kwargs: Any) -> Callable[[_VIEW], _VIEW]: ...
-def list_route(methods: Optional[Sequence[str]] = ..., **kwargs: Any) -> Callable[[_VIEW], _VIEW]: ...
+_LOWER_CASE_HTTP_VERBS = List[
+    Union[
+        Literal["get"],
+        Literal["post"],
+        Literal["delete"],
+        Literal["put"],
+        Literal["patch"],
+        Literal["trace"],
+        Literal["options"],
+    ]
+]
+
+_MIXED_CASE_HTTP_VERBS = List[
+    Union[
+        Literal["GET"],
+        Literal["POST"],
+        Literal["DELETE"],
+        Literal["PUT"],
+        Literal["PATCH"],
+        Literal["TRACE"],
+        Literal["OPTIONS"],
+        Literal["get"],
+        Literal["post"],
+        Literal["delete"],
+        Literal["put"],
+        Literal["patch"],
+        Literal["trace"],
+        Literal["options"],
+    ]
+]
+
+class ViewSetAction:
+    detail: bool
+    methods: _LOWER_CASE_HTTP_VERBS
+    url_path: str
+    url_name: str
+    kwargs: Mapping[str, Any]
+    mapping: MethodMapper
+    def __call__(self, *args, **kwargs): ...
+
+def api_view(http_method_names: Optional[Sequence[str]] = ...) -> Callable[[Callable], AsView]: ...
+def renderer_classes(
+    renderer_classes: Sequence[Union[BaseRenderer, Type[BaseRenderer]]]
+) -> Callable[[Callable], Callable]: ...
+def parser_classes(parser_classes: Sequence[Union[BaseParser, Type[BaseParser]]]) -> Callable[[Callable], Callable]: ...
+def authentication_classes(
+    authentication_classes: Sequence[Union[BaseAuthentication, Type[BaseAuthentication]]]
+) -> Callable[[Callable], Callable]: ...
+def throttle_classes(
+    throttle_classes: Sequence[Union[BaseThrottle, Type[BaseThrottle]]]
+) -> Callable[[Callable], Callable]: ...
+def permission_classes(
+    permission_classes: Sequence[Union[BasePermission, Type[BasePermission]]]
+) -> Callable[[Callable], Callable]: ...
+def schema(view_inspector: Optional[Union[ViewInspector, Type[ViewInspector]]]) -> Callable[[Callable], Callable]: ...
+def action(
+    methods: Optional[_MIXED_CASE_HTTP_VERBS] = ...,
+    detail: bool = ...,
+    url_path: Optional[str] = ...,
+    url_name: Optional[str] = ...,
+    suffix: Optional[str] = ...,
+    name: Optional[str] = ...,
+    **kwargs: Any,
+) -> Callable[[Callable], ViewSetAction]: ...

rest_framework-stubs/parsers.pyi
@@ -1,26 +1,45 @@
-from typing import IO, Any, Dict, Mapping, Optional, Type
+from typing import IO, Any, Dict, Generic, Mapping, Optional, Type, TypeVar
 
-from rest_framework.renderers import BaseRenderer
+from django.core.files.uploadedfile import UploadedFile
+from django.http import QueryDict
+from django.utils.datastructures import MultiValueDict
+from rest_framework.renderers import JSONRenderer
 
-class DataAndFiles(object):
-    data: Any
-    files: Mapping[str, IO[Any]]
-    def __init__(self, data: Any, files: Mapping[str, IO[Any]]) -> None: ...
+_Data = TypeVar("_Data")
+_Files = TypeVar("_Files")
 
-class BaseParser(object):
+class DataAndFiles(Generic[_Data, _Files]):
+    data: _Data
+    files: _Files
+    def __init__(self, data: _Data, files: _Files) -> None: ...
+
+class BaseParser:
     media_type: str = ...
+    def parse(
+        self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
+    ) -> NotImplemented: ...
+
+class JSONParser(BaseParser):
+    renderer_class: Type[JSONRenderer] = ...
+    strict: bool = ...
     def parse(
         self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
     ) -> Dict[str, Any]: ...
 
-class JSONParser(BaseParser):
-    renderer_class: Type[BaseRenderer] = ...
-    strict: bool = ...
+class FormParser(BaseParser):
+    def parse(
+        self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
+    ) -> QueryDict: ...
 
-class FormParser(BaseParser): ...
-class MultiPartParser(BaseParser): ...
+class MultiPartParser(BaseParser):
+    def parse(
+        self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
+    ) -> DataAndFiles[QueryDict, MultiValueDict]: ...
 
 class FileUploadParser(BaseParser):
     errors: Dict[str, str] = ...
+    def parse(
+        self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
+    ) -> DataAndFiles[None, Mapping[str, UploadedFile]]: ...
     def get_filename(self, stream: IO[Any], media_type: Optional[str], parser_context: Mapping[str, Any]) -> str: ...
     def get_encoded_filename(self, filename_parm: Mapping[str, Any]) -> str: ...

commit a0a7e0531357a52b32a89b44548473ede17694cd

Restore functionality lost in version 1.3.0
Fixes for regressions introduced in commit
cf9d492ec01f99e9a3fa89558aa1fbf1a749bc22

rest_framework-stubs/decorators.pyi
@@ -1,5 +1,6 @@
-from typing import Any, Callable, List, Mapping, Optional, Sequence, Type, Union
+from typing import Any, Callable, List, Mapping, Optional, Sequence, Type, Union, Protocol, TypeVar
 
+from django.http.response import HttpResponseBase
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.parsers import BaseParser
 from rest_framework.permissions import BasePermission
@@ -9,17 +10,19 @@ from rest_framework.throttling import BaseThrottle
 from rest_framework.views import APIView, AsView  # noqa: F401
 from typing_extensions import Literal
 
+_View = TypeVar("_View", bound=Callable[..., HttpResponseBase])
+
 class MethodMapper(dict):
-    def __init__(self, action: Callable, methods: Sequence[str]) -> None: ...
-    def _map(self, method: str, func: Callable) -> Callable: ...
-    def get(self, func: Callable) -> Callable: ...  # type: ignore
-    def post(self, func: Callable) -> Callable: ...
-    def put(self, func: Callable) -> Callable: ...
-    def patch(self, func: Callable) -> Callable: ...
-    def delete(self, func: Callable) -> Callable: ...
-    def head(self, func: Callable) -> Callable: ...
-    def options(self, func: Callable) -> Callable: ...
-    def trace(self, func: Callable) -> Callable: ...
+    def __init__(self, action: _View, methods: Sequence[str]) -> None: ...
+    def _map(self, method: str, func: _View) -> _View: ...
+    def get(self, func: _View) -> _View: ...  # type: ignore
+    def post(self, func: _View) -> _View: ...
+    def put(self, func: _View) -> _View: ...
+    def patch(self, func: _View) -> _View: ...
+    def delete(self, func: _View) -> _View: ...
+    def head(self, func: _View) -> _View: ...
+    def options(self, func: _View) -> _View: ...
+    def trace(self, func: _View) -> _View: ...
 
 _LOWER_CASE_HTTP_VERBS = List[
     Literal[
@@ -52,30 +55,30 @@ _MIXED_CASE_HTTP_VERBS = List[
     ]
 ]
 
-class ViewSetAction:
+class ViewSetAction(Protocol[_View]):
     detail: bool
     methods: _LOWER_CASE_HTTP_VERBS
     url_path: str
     url_name: str
     kwargs: Mapping[str, Any]
     mapping: MethodMapper
-    def __call__(self, *args, **kwargs): ...
+    __call__: _View
 
-def api_view(http_method_names: Optional[Sequence[str]] = ...) -> Callable[[Callable], AsView]: ...
+def api_view(http_method_names: Optional[Sequence[str]] = ...) -> Callable[[_View], AsView[_View]]: ...
 def renderer_classes(
     renderer_classes: Sequence[Union[BaseRenderer, Type[BaseRenderer]]]
-) -> Callable[[Callable], Callable]: ...
-def parser_classes(parser_classes: Sequence[Union[BaseParser, Type[BaseParser]]]) -> Callable[[Callable], Callable]: ...
+) -> Callable[[_View], _View]: ...
+def parser_classes(parser_classes: Sequence[Union[BaseParser, Type[BaseParser]]]) -> Callable[[_View], _View]: ...
 def authentication_classes(
     authentication_classes: Sequence[Union[BaseAuthentication, Type[BaseAuthentication]]]
-) -> Callable[[Callable], Callable]: ...
+) -> Callable[[_View], _View]: ...
 def throttle_classes(
     throttle_classes: Sequence[Union[BaseThrottle, Type[BaseThrottle]]]
-) -> Callable[[Callable], Callable]: ...
+) -> Callable[[_View], _View]: ...
 def permission_classes(
     permission_classes: Sequence[Union[BasePermission, Type[BasePermission]]]
-) -> Callable[[Callable], Callable]: ...
-def schema(view_inspector: Optional[Union[ViewInspector, Type[ViewInspector]]]) -> Callable[[Callable], Callable]: ...
+) -> Callable[[_View], _View]: ...
+def schema(view_inspector: Optional[Union[ViewInspector, Type[ViewInspector]]]) -> Callable[[_View], _View]: ...
 def action(
     methods: Optional[_MIXED_CASE_HTTP_VERBS] = ...,
     detail: bool = ...,
@@ -84,4 +87,4 @@ def action(
     suffix: Optional[str] = ...,
     name: Optional[str] = ...,
     **kwargs: Any,
-) -> Callable[[Callable], ViewSetAction]: ...
+) -> Callable[[_View], ViewSetAction[_View]]: ...

rest_framework-stubs/fields.pyi
@@ -28,7 +28,6 @@ from typing import (
 
 from django.core.files.base import File
 from django.db import models
-from django.db.models import Model
 from django.forms import ImageField as DjangoImageField  # noqa: F401
 from typing_extensions import Literal, Final
 
@@ -748,8 +747,8 @@ class JSONField(
         allow_null: bool = ...,
         *,
         binary: bool = ...,
-        encoder: JSONEncoder = ...,
-        decoder: JSONDecoder = ...,
+        encoder: Optional[JSONEncoder] = ...,
+        decoder: Optional[JSONDecoder] = ...,
     ): ...
 
 class ReadOnlyField(Field): ...

rest_framework-stubs/parsers.pyi
@@ -1,4 +1,4 @@
-from typing import IO, Any, Dict, Generic, Mapping, Optional, Type, TypeVar
+from typing import IO, Any, Dict, Generic, Mapping, Optional, Type, TypeVar, Union
 
 from django.core.files.uploadedfile import UploadedFile
 from django.http import QueryDict
@@ -17,7 +17,7 @@ class BaseParser:
     media_type: str = ...
     def parse(
         self, stream: IO[Any], media_type: Optional[str] = ..., parser_context: Optional[Mapping[str, Any]] = ...
-    ): ...
+    ) -> Union[Mapping[Any, Any], DataAndFiles]: ...
 
 class JSONParser(BaseParser):
     renderer_class: Type[JSONRenderer] = ...

rest_framework-stubs/relations.pyi
@@ -16,6 +16,7 @@ class Hyperlink(str):
     @property
     def name(self) -> str: ...
     is_hyperlink: bool = ...
+    obj: Any
 
 class PKOnlyObject:
     pk: Any = ...

rest_framework-stubs/response.pyi
@@ -1,11 +1,11 @@
-from typing import Any, Dict, List, Mapping, Optional, Union
+from typing import Any, Mapping, Optional
 
 from django.template.response import SimpleTemplateResponse
 
 from rest_framework.request import Request
 
 class Response(SimpleTemplateResponse):
-    data: Optional[Union[List[Dict[str, Any]], Dict[str, Any]]] = ...
+    data: Any = ...
     exception: bool = ...
     content_type: Optional[str] = ...
     _request: Request
@@ -19,6 +19,7 @@ class Response(SimpleTemplateResponse):
         content_type: Optional[str] = ...,
     ): ...
     @property
-    def rendered_content(self): ...
+    def rendered_content(self) -> Any: ...
+    def render(self) -> Any: ...
     @property
     def status_text(self) -> str: ...

rest_framework-stubs/schemas/utils.pyi
@@ -1,6 +1,8 @@
+from typing import Type
+
 from django.db.models import Model
 from rest_framework.fields import Field
 from rest_framework.views import APIView
 
 def is_list_view(path: str, method: str, view: APIView) -> bool: ...
-def get_pk_description(model: Model, model_field: Field) -> str: ...
+def get_pk_description(model: Type[Model], model_field: Field) -> str: ...

rest_framework-stubs/views.pyi
@@ -1,6 +1,7 @@
-from typing import Any, Callable, Dict, List, Mapping, NoReturn, Optional, Sequence, Tuple, Type
+from typing import Any, Callable, Dict, List, Mapping, NoReturn, Optional, Sequence, Tuple, Type, Protocol, TypeVar
 
 from django.http import HttpRequest
+from django.http.response import HttpResponseBase
 from django.views.generic import View
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.negotiation import BaseContentNegotiation
@@ -19,11 +20,17 @@ def get_view_description(view: APIView, html: bool = ...) -> str: ...
 def set_rollback() -> None: ...
 def exception_handler(exc: Exception, context) -> Optional[Response]: ...
 
-class AsView(Response):
+_View = TypeVar("_View", bound=Callable[..., HttpResponseBase])
+
+class AsView(Protocol[_View]):
     self: APIView
     view_class: Type[APIView]
     view_initkwargs: Mapping[str, Any]
-    def __call__(self, *args, **kwargs): ...
+    __call__: _View
+
+# Call signature for view function that's returned by as_view()
+class GenericView(Protocol):
+    def __call__(self, request: HttpRequest, *args: Any, **kwargs: Any) -> Response: ...
 
 class APIView(View):
     args: Any = ...
@@ -47,7 +54,7 @@ class APIView(View):
     @property
     def default_response_headers(self) -> Dict[str, str]: ...
     @classmethod
-    def as_view(cls, **initkwargs: Any) -> AsView: ...
+    def as_view(cls, **initkwargs: Any) -> AsView[GenericView]: ...
     def http_method_not_allowed(self, request: Request, *args: Any, **kwargs: Any) -> Response: ...  # type: ignore[override]
     def permission_denied(self, request: Request, message: Optional[str] = ...) -> NoReturn: ...
     def throttled(self, request: Request, wait: float) -> NoReturn: ...

rest_framework-stubs/viewsets.pyi
@@ -5,7 +5,7 @@ from django.http.response import HttpResponseBase
 from rest_framework import generics, mixins, views
 from rest_framework.decorators import ViewSetAction
 from rest_framework.request import Request
-from rest_framework.views import AsView
+from rest_framework.views import AsView, GenericView
 
 def _is_extra_action(attr: Any) -> bool: ...
 
@@ -25,7 +25,9 @@ class ViewSetMixin:
     # Assigned in initialize_request()
     action: str
     @classmethod
-    def as_view(cls, actions: Optional[Dict[str, Union[str, ViewSetAction]]] = ..., **initkwargs: Any) -> AsView: ...
+    def as_view(
+        cls, actions: Optional[Dict[str, Union[str, ViewSetAction]]] = ..., **initkwargs: Any
+    ) -> AsView[GenericView]: ...
     def initialize_request(self, request: Request, *args: Any, **kwargs: Any) -> Request: ...
     def reverse_action(self, url_name: str, *args: Any, **kwargs: Any) -> str: ...
     @classmethod

scripts/typecheck_tests.py
@@ -55,10 +55,6 @@ IGNORED_ERRORS = {
     "authentication": [
         'Argument 1 to "post" of "APIClient" has incompatible type "None"; expected "str',
         ' base class "BaseTokenAuthTests" defined the type as "None"',
-        'No overload variant of "__getitem__" of "list" matches argument type "str"',
-        "is not indexable",
-        "def __getitem__",
-        "Possible overload variant",
         '"None" has no attribute "objects"',
         '"BaseTokenAuthTests" has no attribute "assertNumQueries"',
     ],
@@ -75,6 +71,7 @@ IGNORED_ERRORS = {
         'error: Module has no attribute "coreapi"',
         'Value of type "Optional[str]" is not indexable',
         "Module 'rest_framework.compat' has no attribute 'yaml'",
+        'Incompatible types in assignment (expression has type "AsView[GenericView]", variable has type "AsView[Callable[[Any], Any]]")',  # noqa: E501
     ],
     "browsable_api": [
         '(expression has type "List[Dict[str, Dict[str, int]]]", base class "GenericAPIView" defined the type as "Union[QuerySet[_MT?], Manager[_MT?], None]")',  # noqa: E501
@@ -90,7 +87,7 @@ IGNORED_ERRORS = {
     "test_bound_fields.py": ['Value of type "BoundField" is not indexable'],
     "test_decorators.py": [
         'Argument 1 to "api_view" has incompatible type "Callable[[Any], Any]"; expected "Optional[Sequence[str]]"',
-        '"AsView" has no attribute "cls"',
+        '"AsView[Callable[[Any], Any]]" has no attribute "cls"',
     ],
     "test_encoders.py": ['Argument "serializer" to "ReturnList" has incompatible type "None'],
     "test_fields.py": [
@@ -212,6 +209,9 @@ IGNORED_ERRORS = {
         'Argument 1 to "to_internal_value" of "Field" has incompatible type "object"',
         'Argument "data" to "ValidationSerializer" has incompatible type "str"; expected "Mapping[str, Any]"',
         'Argument "data" to "ValidationSerializer" has incompatible type "str"',
+    ],
+    "test_validation_error.py": [
+        'Argument "detail" to "ValidationError" has incompatible type "Tuple[str, str]"; expected "Union[str, List[Any], Dict[str, Any], None]"',  # noqa: E501
     ],
     "test_validators.py": [
         'has incompatible type "object"; expected "QuerySet[Any]"',

tests/typecheck/test_decorators.yml
@@ -3,7 +3,7 @@
         from rest_framework.decorators import api_view
         @api_view()
         def view_func1(request): ...
-        reveal_type(view_func1)  # N: Revealed type is 'rest_framework.views.AsView'
+        reveal_type(view_func1)  # N: Revealed type is 'rest_framework.views.AsView[def (request: Any) -> Any]'
 -   case: api_view_fancy
     main: |
         from rest_framework.decorators import api_view
@@ -11,9 +11,16 @@
         from rest_framework.response import Response
         @api_view(['GET', 'POST'])
         def view_func2(request: Request, arg: str) -> Response: ...
-        reveal_type(view_func2)  # N: Revealed type is 'rest_framework.views.AsView'
+        reveal_type(view_func2)  # N: Revealed type is 'rest_framework.views.AsView[def (request: rest_framework.request.Request, arg: builtins.str) -> rest_framework.response.Response]'
+
+        view_func2(None, 10)  # E: Argument 1 to "view_func2" has incompatible type "None"; expected "Request"  # E: Argument 2 to "view_func2" has incompatible type "int"; expected "str"
 -   case: api_view_bare_is_error
     main: |
         from rest_framework.decorators import api_view
         @api_view  # E: Argument 1 to "api_view" has incompatible type "Callable[[Any], Any]"; expected "Optional[Sequence[str]]"
         def view_func2(request): ...
+-   case: api_view_incorrect_return
+    main: |
+        from rest_framework.decorators import api_view
+        @api_view()  # E: Value of type variable "_View" of function cannot be "Callable[[Any], List[Any]]"
+        def view_func2(request) -> list: ...

tests/typecheck/test_fields.yml
@@ -49,7 +49,7 @@
 
         JSONField(True, True, True, {}, {}, 'src', 'l', 'ht', {}, {}, [], True)
         JSONField(True, True, True, {}, {}, 'src', 'l', 'ht', {}, {}, [], True, True)  # E: Too many positional arguments for "JSONField"
-        JSONField(True, True, True, {}, {}, 'src', 'l', 'ht', {}, {}, [], True, binary=True)
+        JSONField(True, True, True, {}, {}, 'src', 'l', 'ht', {}, {}, [], True, binary=True, encoder=None, decoder=None)
 
 -   case: all_positional_args_fields
     main: |