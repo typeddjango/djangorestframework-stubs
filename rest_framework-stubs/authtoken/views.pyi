5511807cab60d26d6e62a796cc61472fd5e162f0
Marti Raudsepp <marti@juffo.org>
2021-01-28 21:23:52 +0200

    Fixes for ObtainAuthToken (#128)

--- rest_framework-stubs/authtoken/views.pyi
+++ rest_framework-stubs/authtoken/views.pyi
@@ -1,12 +1,12 @@
-from typing import Any
+from typing import Any, Type
 
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView, AsView
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView, AsView, GenericView
 
 class ObtainAuthToken(APIView):
-    serializer_class = AuthTokenSerializer
+    serializer_class: Type[Serializer] = ...
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response: ...
 
-obtain_auth_token: AsView = ...
+obtain_auth_token: AsView[GenericView] = ...
