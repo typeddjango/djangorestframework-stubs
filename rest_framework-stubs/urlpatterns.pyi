d1435cce7715e21dc021f9e7add34ea443de59fd
Peter Schutt <peter@topsport.com.au>
2022-01-23 20:30:02 +1000

    Covariant type parameters for format_suffix_patterns.
    Define `urlpatterns` as `Iterable`, they are only iterated over inside
    `apply_suffix_patterns`.
    
    Define `allowed` as `Optional[Sequence[...]]` instead as it is documented as tuple/list
    and needs to support `__len__` and `__getitem__`.

--- rest_framework-stubs/urlpatterns.pyi
+++ rest_framework-stubs/urlpatterns.pyi
@@ -1,15 +1,15 @@
-from typing import List, Optional, Pattern, Union
+from typing import Iterable, List, Optional, Pattern, Sequence, Union
 
 from django.urls.resolvers import RoutePattern, URLPattern, URLResolver
 
 def apply_suffix_patterns(
-    urlpatterns: List[Union[URLResolver, RoutePattern, URLPattern, Pattern]],
+    urlpatterns: Iterable[Union[URLResolver, RoutePattern, URLPattern, Pattern]],
     suffix_pattern: Union[str, Pattern],
     suffix_required: bool,
     suffix_route: Optional[str] = ...,
 ) -> List[URLPattern]: ...
 def format_suffix_patterns(
-    urlpatterns: List[Union[URLResolver, RoutePattern, URLPattern, Pattern]],
+    urlpatterns: Iterable[Union[URLResolver, RoutePattern, URLPattern, Pattern]],
     suffix_required: bool = ...,
-    allowed: Optional[List[Union[URLPattern, Pattern, str]]] = ...,
+    allowed: Optional[Sequence[Union[URLPattern, Pattern, str]]] = ...,
 ) -> List[URLPattern]: ...
