5f6059d1ecedad6c42b0a0f74703d5a39b02446d
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-05-24 12:06:21 +0200

    bump to 1.6.0 (#222)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ dependencies = [
 
 setup(
     name="djangorestframework-stubs",
-    version="1.5.0",
+    version="1.6.0",
     description="PEP-484 stubs for django-rest-framework",
     long_description=readme,
     long_description_content_type="text/markdown",
