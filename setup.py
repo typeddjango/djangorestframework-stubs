aed18871e836ce2eae6b74067eb639ede9ecf311
sobolevn <mail@sobolevn.me>
2022-06-22 12:15:01 +0300

    Version 1.7.0 release, python3.10 support, closes #234

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extras_require = {
 
 setup(
     name="djangorestframework-stubs",
-    version="1.6.0",
+    version="1.7.0",
     description="PEP-484 stubs for django-rest-framework",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Typing :: Typed",
     ],
 )
