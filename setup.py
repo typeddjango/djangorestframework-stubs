21818bc6335601e10289078974d6a7e9beb7d4c5
Marti Raudsepp <marti@juffo.org>
2023-02-22 13:00:19 +0200

    Version 1.9.0 release (#357)
    * Version 1.9.0 release
    * Bump django-stubs requirement

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extras_require = {
 
 setup(
     name="djangorestframework-stubs",
-    version="1.8.0",
+    version="1.9.0",
     description="PEP-484 stubs for django-rest-framework",
     long_description=readme,
     long_description_content_type="text/markdown",
