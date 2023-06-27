7161f47dbfc804a78b3260c0cff13379998199c4
Marti Raudsepp <marti@juffo.org>
2023-06-27 18:29:43 +0300

    CI: Remove typecheck tests and clean things up
    * Removed everything related to `typecheck_tests.py`
    * Made true/false values lowercase in `mypy.ini` like django-stubs

--- scripts/paths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-PROJECT_DIRECTORY = Path(__file__).parent.parent
-STUBS_DIRECTORY = PROJECT_DIRECTORY / "rest_framework-stubs"  # type: Path
-DRF_SOURCE_DIRECTORY = PROJECT_DIRECTORY / "drf_source"  # type: Path
-STUBGEN_TARGET_DIRECTORY = PROJECT_DIRECTORY / "stubgen"  # type: Path
