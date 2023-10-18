41028f31a9e7e3c42815aea65a82ae53df2dffde
Marti Raudsepp <marti@juffo.org>
2023-10-18 17:54:01 +0300

    Initial stubtest testing with autogenerated allowlist (#493)
    * Initial stubtest testing with autogenerated allowlist
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    * Update CONTRIBUTING.md
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ scripts/stubtest.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Run this script as `bash ./scripts/stubtest.sh`
+
+set -e
+
+export MYPYPATH='.'
+
+# TODO: remove `--ignore-positional-only` when ready
+stubtest rest_framework \
+    --mypy-config-file mypy.ini \
+    --ignore-positional-only \
+    --allowlist scripts/stubtest/allowlist.txt \
+    --allowlist scripts/stubtest/allowlist_todo.txt
