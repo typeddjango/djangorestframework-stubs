679667c6ab5acdf944e58c795c4b90a19f35537a
q0w <43147888+q0w@users.noreply.github.com>
2024-05-27 15:49:42 +0300

    Accept Promise as Validator.message|missing_message

--- rest_framework-stubs/validators.pyi
+++ rest_framework-stubs/validators.pyi
@@ -2,6 +2,7 @@ from collections.abc import Callable, Container, Iterable, MutableMapping
 from typing import Any, Protocol, TypeVar
 
 from django.db.models import Model, QuerySet
+from django.utils.functional import _StrOrPromise
 from rest_framework.fields import Field
 from rest_framework.serializers import BaseSerializer
 from typing_extensions import TypeAlias
@@ -19,22 +20,22 @@ def qs_exists(queryset: QuerySet) -> bool: ...
 def qs_filter(queryset: QuerySet[_T], **kwargs: Any) -> QuerySet[_T]: ...
 
 class UniqueValidator:
-    message: str
+    message: _StrOrPromise
     requires_context: bool
     queryset: QuerySet
     lookup: str
-    def __init__(self, queryset: QuerySet, message: str | None = ..., lookup: str = ...) -> None: ...
+    def __init__(self, queryset: QuerySet, message: _StrOrPromise | None = ..., lookup: str = ...) -> None: ...
     def filter_queryset(self, value: Any, queryset: QuerySet[_T], field_name: str) -> QuerySet[_T]: ...
     def exclude_current_instance(self, queryset: QuerySet[_T], instance: _T) -> QuerySet[_T]: ...
     def __call__(self, value: Any, serializer_field: Field) -> None: ...
 
 class UniqueTogetherValidator:
-    message: str
-    missing_message: str
+    message: _StrOrPromise
+    missing_message: _StrOrPromise
     requires_context: bool
     queryset: QuerySet
     fields: Iterable[str]
-    def __init__(self, queryset: QuerySet, fields: Iterable[str], message: str | None = ...) -> None: ...
+    def __init__(self, queryset: QuerySet, fields: Iterable[str], message: _StrOrPromise | None = ...) -> None: ...
     def enforce_required_fields(self, attrs: Container[str], serializer: BaseSerializer) -> None: ...
     def filter_queryset(
         self, attrs: MutableMapping[str, Any], queryset: QuerySet[_T], serializer: BaseSerializer
@@ -45,18 +46,20 @@ class UniqueTogetherValidator:
     def __call__(self, attrs: MutableMapping[str, Any], serializer: BaseSerializer) -> None: ...
 
 class ProhibitSurrogateCharactersValidator:
-    message: str
+    message: _StrOrPromise
     code: str
     def __call__(self, value: Any) -> None: ...
 
 class BaseUniqueForValidator:
-    message: str
-    missing_message: str
+    message: _StrOrPromise
+    missing_message: _StrOrPromise
     requires_context: bool
     queryset: QuerySet
     field: str
     date_field: str
-    def __init__(self, queryset: QuerySet, field: str, date_field: str, message: str | None = ...) -> None: ...
+    def __init__(
+        self, queryset: QuerySet, field: str, date_field: str, message: _StrOrPromise | None = ...
+    ) -> None: ...
     def enforce_required_fields(self, attrs: Container[str]) -> None: ...
     def filter_queryset(
         self, attrs: MutableMapping[str, Any], queryset: QuerySet[_T], field_name: str, date_field_name: str
