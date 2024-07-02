4e3de71d12aafe93f5742d636b933de38b8e7112
DongwookKim0823 <dwkim0823@naver.com>
2024-07-03 03:11:51 +0900

    Add type hints to JSON utility methods in rest_framework.utils

--- rest_framework-stubs/utils/json.pyi
+++ rest_framework-stubs/utils/json.pyi
@@ -1,7 +1,55 @@
-from typing import Any, NoReturn
+from json import JSONDecoder, JSONEncoder
+from typing import IO, Any, Callable, NoReturn
 
 def strict_constant(o: Any) -> NoReturn: ...
-def dump(*args: Any, **kwargs: Any) -> None: ...
-def dumps(*args: Any, **kwargs: Any) -> str: ...
-def load(*args: Any, **kwargs: Any) -> Any: ...
-def loads(*args: Any, **kwargs: Any) -> Any: ...
+def dump(
+    obj: Any,
+    fp: IO[str],
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: int | str | None = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[Any], Any] | None = None,
+    sort_keys: bool = False,
+    **kw: Any,
+) -> None: ...
+def dumps(
+    obj: Any,
+    *,
+    skipkeys: bool = False,
+    ensure_ascii: bool = True,
+    check_circular: bool = True,
+    allow_nan: bool = True,
+    cls: type[JSONEncoder] | None = None,
+    indent: int | str | None = None,
+    separators: tuple[str, str] | None = None,
+    default: Callable[[Any], Any] | None = None,
+    sort_keys: bool = False,
+    **kw: Any,
+) -> str: ...
+def load(
+    fp: IO[str],
+    *,
+    cls: type[JSONDecoder] | None = None,
+    object_hook: Callable[[dict[str, Any]], Any] | None = None,
+    parse_float: Callable[[str], Any] | None = None,
+    parse_int: Callable[[str], Any] | None = None,
+    parse_constant: Callable[[str], Any] | None = None,
+    object_pairs_hook: Callable[[list[tuple[str, Any]]], Any] | None = None,
+    **kw: Any,
+) -> Any: ...
+def loads(
+    s: str,
+    *,
+    cls: type[JSONDecoder] | None = None,
+    object_hook: Callable[[dict[str, Any]], Any] | None = None,
+    parse_float: Callable[[str], Any] | None = None,
+    parse_int: Callable[[str], Any] | None = None,
+    parse_constant: Callable[[str], Any] | None = None,
+    object_pairs_hook: Callable[[list[tuple[str, Any]]], Any] | None = None,
+    **kw: Any,
+) -> Any: ...
