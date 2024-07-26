94a9e0339e167f0326d866518af579871f14c0ed
sobolevn <mail@sobolevn.me>
2024-07-26 13:11:57 +0300

    Add `objects` attribute to `Token` (#644)

--- rest_framework-stubs/authtoken/models.pyi
+++ rest_framework-stubs/authtoken/models.pyi
@@ -1,12 +1,14 @@
-from typing import Any
+from typing import Any, ClassVar
 
 from django.db import models
+from django.db.models.manager import Manager
 from typing_extensions import Self
 
 class Token(models.Model):
     key: models.CharField
     user: models.OneToOneField
     created: models.DateTimeField
+    objects: ClassVar[Manager[Self]]
     @classmethod
     def generate_key(cls) -> str: ...
 
