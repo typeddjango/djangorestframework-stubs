47e050afc2ec7163c9844f0e06dfd7687fe60f62
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-24 10:19:56 +0200

    Fix incorrect function signatures in documentation and schemas stubs (#909)
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- rest_framework-stubs/documentation.pyi
+++ rest_framework-stubs/documentation.pyi
@@ -1,43 +1,44 @@
 from collections.abc import Callable, Sequence
 from typing import Any
 
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import _PermissionClass
 from rest_framework.renderers import BaseRenderer
 from rest_framework.schemas import SchemaGenerator
 from rest_framework.urlpatterns import _AnyURL
 
 def get_docs_view(
     title: str | None = ...,
-    url: str | None = ...,
     description: str | None = ...,
+    schema_url: str | None = ...,
     urlconf: str | None = ...,
-    renderer_classes: Sequence[type[BaseRenderer]] | None = ...,
     public: bool = ...,
     patterns: Sequence[_AnyURL] | None = ...,
     generator_class: type[SchemaGenerator] = ...,
-    authentication_classes: Sequence[str] = ...,
-    permission_classes: Sequence[str] = ...,
+    authentication_classes: Sequence[type[BaseAuthentication]] = ...,
+    permission_classes: Sequence[_PermissionClass] = ...,
+    renderer_classes: Sequence[type[BaseRenderer]] | None = ...,
 ) -> Callable[..., Any]: ...
 def get_schemajs_view(
     title: str | None = ...,
-    url: str | None = ...,
     description: str | None = ...,
+    schema_url: str | None = ...,
     urlconf: str | None = ...,
-    renderer_classes: Sequence[type[BaseRenderer]] | None = ...,
     public: bool = ...,
     patterns: Sequence[_AnyURL] | None = ...,
     generator_class: type[SchemaGenerator] = ...,
-    authentication_classes: Sequence[str] = ...,
-    permission_classes: Sequence[str] = ...,
+    authentication_classes: Sequence[type[BaseAuthentication]] = ...,
+    permission_classes: Sequence[_PermissionClass] = ...,
 ) -> Callable[..., Any]: ...
 def include_docs_urls(
     title: str | None = ...,
-    url: str | None = ...,
     description: str | None = ...,
+    schema_url: str | None = ...,
     urlconf: str | None = ...,
-    renderer_classes: Sequence[type[BaseRenderer]] | None = ...,
     public: bool = ...,
     patterns: Sequence[_AnyURL] | None = ...,
     generator_class: type[SchemaGenerator] = ...,
-    authentication_classes: Sequence[str] = ...,
-    permission_classes: Sequence[str] = ...,
+    authentication_classes: Sequence[type[BaseAuthentication]] = ...,
+    permission_classes: Sequence[_PermissionClass] = ...,
+    renderer_classes: Sequence[type[BaseRenderer]] | None = ...,
 ) -> Any: ...
