a6041e6b18cab5c8fd9119fc1e98aaa22926c5b1
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-02-27 02:55:17 +0200

    Fix incomplete types in schemas stubs
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- rest_framework-stubs/schemas/views.pyi
+++ rest_framework-stubs/schemas/views.pyi
@@ -1,5 +1,7 @@
+from collections.abc import Sequence
 from typing import Any
 
+from rest_framework.renderers import BaseRenderer
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.schemas import SchemaGenerator
@@ -9,5 +11,7 @@ class SchemaView(APIView):
     _ignore_model_permissions: bool
     public: bool
     schema_generator: SchemaGenerator | None
+    renderer_classes: Sequence[type[BaseRenderer]] | None  # type: ignore[assignment]
 
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response: ...
