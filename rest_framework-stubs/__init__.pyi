40c4fa32cd647be6b0c1ad0ceca0d1261c5d8707
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2026-05-22 16:27:06 +0300

    Clean up and improve `rest_framework.fields`
    Signed-off-by: Emmanuel Ferdman <emmanuelferdman@gmail.com>

--- rest_framework-stubs/__init__.pyi
+++ rest_framework-stubs/__init__.pyi
@@ -7,6 +7,4 @@ __copyright__: str
 VERSION: str
 HTTP_HEADER_ENCODING: str
 ISO_8601: str
-default_app_config: str
-
-class RemovedInDRF317Warning(PendingDeprecationWarning): ...
+DJANGO_DURATION_FORMAT: str
